d108aa8357afdf15173ac7b0ee5e082d23569f17
Bartek <b.kowalski@qaunit.com>
2020-05-18 08:48:48 +0200

    back to old browser

--- IEH/nitro+bank.sh
+++ IEH/nitro+bank.sh
@@ -2,7 +2,7 @@
 
 sleep 0.3;
 
-for i in {1..1000};
+for i in {1..100000};
 do 
 
 # PERFORMANCE MODE ON
@@ -15,7 +15,7 @@ xdotool click --delay 300 1;
 xdotool mousemove 440 420;
 
 
-sleep 1800;
+sleep 600;
 # PERFORMANCE MODE OFF
 sleep 0.3;
 xdotool mousemove 705 940;
@@ -28,14 +28,12 @@ xdotool click --delay 300 1;
 sleep 0.3;
 xdotool mousemove 450 940;
 xdotool click --delay 300 1;
-xdotool mousemove 530 520;
+xdotool mousemove 525 525;
 xdotool click --delay 300 1;
 xdotool mousemove 435 550;
 xdotool click --delay 300 1;
 xdotool mousemove 620 640;
-xdotool key --repeat 18 --delay 500 --repeat-delay 1000 M+U
-
-xdotool mousemove 440 420;
+xdotool key --repeat 6 --delay 500 --repeat-delay 1000 M+U
 
 
 # SLIME BANK CAP
